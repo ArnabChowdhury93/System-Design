5fd768afaf01dba00200ff07401d5e9fc5fb1c10
ArnabChowdhury93 <arnabakanandan@gmail.com>
2020-09-12 21:18:16 +0530

    amz initializer commit

---
+++ LLD/amz/src/test/java/com/eshop/amz/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.amz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
