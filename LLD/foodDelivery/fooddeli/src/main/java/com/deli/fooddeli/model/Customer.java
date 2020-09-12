2b16d3242446758a144afa3147a4d1d2e3d346af
ArnabChowdhury93 <arnabakanandan@gmail.com>
2020-09-13 02:29:51 +0530

    zxcd

--- LLD/foodDelivery/fooddeli/src/main/java/com/deli/fooddeli/model/Customer.java
+++
@@ -1,8 +0,0 @@
-package com.deli.fooddeli.model;
-
-public abstract class Customer {
-    int customerID;
-    Cart cart;
-    Sear
-    
-}
--- LLD/foodDelivery/fooddeli/src/main/java/com/deli/fooddeli/model/Guest.java
+++
@@ -1,5 +0,0 @@
-package com.deli.fooddeli.model;
-
-public class Guest extends Customer{
-    public Account createAccount();
-}
